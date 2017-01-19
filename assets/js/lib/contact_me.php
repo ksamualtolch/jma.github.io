8ea5d5c95e5201a5a96275b13bd1c93d8c194aaa
ksamualtolch <ksamualtolch@gmail.com>
2017-01-19 00:57:50 -0800

    Form experiment

--- assets/js/lib/contact_me.php
+++ assets/js/lib/contact_me.php
@@ -22,7 +22,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'ksamualtolch@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message via KEITHTOLCH.COM.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nBusiness: $business\n\nIndustry: $industry\n\nMessage:\n$message";
+$email_body = "You have received a new message via JMA.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nBusiness: $business\n\nIndustry: $industry\n\nMessage:\n$message";
 $headers = "From: noreply@keithtolch.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
