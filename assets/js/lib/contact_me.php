42bc7c74f58e642e599f630b23e8439c7e9a9c44
ksamualtolch <ksamualtolch@gmail.com>
2017-01-19 00:15:05 -0800

    New form php attempt

--- assets/js/lib/contact_me.php
+++ assets/js/lib/contact_me.php
@@ -1,10 +1,7 @@
 <?php
 // Check for empty fields
 if(empty($_POST['name'])  		||
-   empty($_POST['phone'])  		||	
    empty($_POST['email']) 		||
-   empty($_POST['business'])  		||
-   empty($_POST['industry'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -13,16 +10,14 @@ if(empty($_POST['name'])  		||
    }
 	
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$businessName = strip_tags(htmlspecialchars($_POST['business']));
-$industryType = strip_tags(htmlspecialchars($_POST['industry']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
 $to = 'ksamualtolch@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message via JMA Marketing\n\n"."Here are the details:\n\nName: $name\n\nPhone Number: $phone\n\nEmail: $email_address\n\nBusiness Name: $business\n\nIndustry Type: $industry\n\nMessage:\n$message";
+$email_body = "You have received a new message via KEITHTOLCH.COM.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@keithtolch.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
