0b2c015b48b757b1cebfcbe96eda0edd55efdeed
ksamualtolch <ksamualtolch@gmail.com>
2017-03-09 11:39:30 -0800

    Tweak
    Added bot killer

--- assets/js/lib/mail.php
+++ assets/js/lib/mail.php
@@ -1,4 +1,9 @@
 <?php
+
+if(isset($_POST['title']) && !empty($_POST['title'])) {
+	die();
+} else {
+
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
@@ -6,12 +11,16 @@ $business = $_POST['business'];
 $industry = $_POST['industry'];
 $message = $_POST['message'];
 
+
 $formcontent = "You have received a new message via JMA Marketing.\n\n"."Here are the details:\n\nName: $name\n\nPhone Number: $phone\n\nEmail: $email\n\nBusiness Name: $business\n\nIndustry Type: $industry\n\nMessage:\n$message";
 $recipient = "ksamualtolch@gmail.com";
 $subject = "A New Message From JMA.COM";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header("Location: ../../../thankyou.html");
+
+}
+
 ?>
 
 <!-- echo "Thank You!"; -->
